b46c0a32f1c635282dd3358f391ab72f0b24b354
Şamil NART <samil.nart@gmail.com>
2018-12-02 02:03:53 +0300

    Use AsyncAction

--- app/src/main/java/sigsegv/com/health/UserListActivity.kt
+++ app/src/main/java/sigsegv/com/health/UserListActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_user_list.*
 import sigsegv.com.health.api.getAllUsers
 
@@ -17,6 +19,8 @@ class UserListActivity : AppCompatActivity(), OnItemClickListener {
         val rvUsers = rv_user_list
         val users = UserList.createMockObjects(20)
         val adapter = UserListAdapter(users, this)
+
+        AsyncAction({ getAllUsers(this@UserListActivity) }, { Log.wtf("Hello", Gson().toJson(it)) })
 
         rvUsers.adapter = adapter
         rvUsers.layoutManager = LinearLayoutManager(this)
