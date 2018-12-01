f8a1a00e1ce170d22c77db6ce0a845b3da645c58
hbusul <hamdiburak.usul@agu.edu.tr>
2018-12-01 22:09:15 +0300

    UserOverview Activity Mock

---
+++ app/src/main/java/sigsegv/com/health/UserOverviewActivity.kt
@@ -0,0 +1,12 @@
+package sigsegv.com.health
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserOverviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_overview)
+    }
+}
