386abcf891a268425f1b42470aee3f626df27dd5
hbusul <hamdiburak.usul@agu.edu.tr>
2018-12-02 11:59:45 +0300

    code cleanup UserListActivity, UserListAdapter

--- app/src/main/java/sigsegv/com/health/UserListActivity.kt
+++ app/src/main/java/sigsegv/com/health/UserListActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
-import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_user_list.*
 import sigsegv.com.health.api.entities.SignInUserData
 import sigsegv.com.health.api.getAllUsers
